a433d055a60b689c032ca8d05260b4b657409895
yangshq <yangshq@pvc123.com>
2021-02-19 09:10:53 +0800

    feat：二叉排序树删除节点思路

--- src/main/java/com/dayi/binarysorttree/BinarySortTreeDemo.java
+++ src/main/java/com/dayi/binarysorttree/BinarySortTreeDemo.java
@@ -2,6 +2,40 @@ package com.dayi.binarysorttree;
 
 /**
  * 二叉排序树
+ * 二叉排序树删除节点（需要区分三种情况）
+ * 1.第一种情况：删除叶子节点
+ *  思路：
+ *  1）需要先找到要删除的节点targetNode
+ *  2) 找到targetNode的父节点parent
+ *  3) 确定targetNode是parent的左子节点，还是右子节点
+ *  4) 根据前面的情况来对应的删除
+ *  parent.left = null;
+ *  parent.right = null;
+ * 2.第二种情况：删除只有一颗子树的节点
+ *  思路：
+ *  1）需要先找到要删除的节点targetNode
+ *  2) 找到targetNode的父节点parent
+ *  3) 确定targetNode的子节点是左子节点还是右子节点
+ *  4）确定targetNode是parent的左子节点还是右子节点
+ *  5）如果targetNode有左子节点
+ *  5.1）如果targetNode是parent的左子节点
+ *    parent.left = targetNode.left
+ *  5.2) 如果targetNode是parent的右子节点
+ *    parent.right = targetNode.left
+ *  6) 如果targetNode有右子节点
+ *  6.1) 如果targetNode是parent的左子节点
+ *    parent.left = targetNode.right
+ *  6.2) 如果targetNode是parent的右子节点
+ *    parent.right = targetNode.right
+ * 3.第三种情况：删除有两颗子树的节点
+ *  思路：
+ *  1）需要先找到要删除的节点targetNode
+ *  2) 找到targetNode的父节点parent
+ *  3）从targetNode的右子树中找到最小的节点
+ *  4) 用一个临时变量，将最小节点的值保存 （例如：temp = 12）
+ *  5) 删除该最小节点
+ *  6) 将临时变量的值，赋值给targetNode，targetNode.value = temp;
+ *
  * @author yangshaoqiang <yangshq@pvc123.com>
  * @create 2021-02-18 16:16
  */
@@ -48,7 +82,6 @@ class BinarySortTree {
             System.out.println("二叉排序树为空，不能遍历");
         }
     }
-
 }
 
 /**
@@ -82,7 +115,7 @@ class Node {
             return;
         }
         // 判断传入的节点值，和当前节点值的关系
-        if (this.value > node.value) {
+        if (node.value < this.value) {
             if (null == this.left) {
                 // 如果当前节点的左子树为空，则直接加入
                 this.left = node;
