534e1a159e1eab0d54a8b7fed0924f322a09e400
yangshq <yangshq@pvc123.com>
2021-02-19 11:45:25 +0800

    feat：二叉排序树删除节点（需要三种情况处理：删除叶子节点，删除有一颗子树的节点，删除有两颗子树的节点）

--- src/main/java/com/dayi/binarysorttree/BinarySortTreeDemo.java
+++ src/main/java/com/dayi/binarysorttree/BinarySortTreeDemo.java
@@ -41,14 +41,30 @@ package com.dayi.binarysorttree;
  */
 public class BinarySortTreeDemo {
     public static void main(String[] args) {
-        int[] arr = {7, 3, 10, 12, 5, 1, 9};
+        int[] arr = {7, 3, 10, 12, 5, 1, 9, 2};
         BinarySortTree binarySortTree = new BinarySortTree();
         // 循环添加节点到二叉排序树
         for (int i = 0; i < arr.length; i++) {
             binarySortTree.add(new Node(arr[i]));
         }
+
         // 中序遍历二叉排序树
         System.out.println("中序遍历二叉排序树");
+        binarySortTree.infixOrder();
+
+        /*// 删除叶子节点测试
+        binarySortTree.delNode(5);
+        System.out.println("删除叶子节点后，中序遍历二叉排序树");
+        binarySortTree.infixOrder();*/
+
+        // 删除只有一个叶子节点的节点测试
+        /*binarySortTree.delNode(1);
+        System.out.println("删除只有一颗子树的节点后，中序遍历二叉树");
+        binarySortTree.infixOrder();*/
+
+        // 删除有两颗子树的节点
+        binarySortTree.delNode(7);
+        System.out.println("删除有两颗子树的节点后，中序遍历二叉树");
         binarySortTree.infixOrder();
     }
 }
@@ -82,6 +98,117 @@ class BinarySortTree {
             System.out.println("二叉排序树为空，不能遍历");
         }
     }
+
+    /**
+     * 查找要删除的节点
+     * @param value
+     * @return
+     */
+    public Node search(int value) {
+        if (null == root) {
+            return null;
+        }
+        return root.search(value);
+    }
+
+    /**
+     * 查找父节点
+     * @param value
+     * @return
+     */
+    public Node searchParent(int value) {
+        if (null == root) {
+            return null;
+        }
+        return root.searchParent(value);
+    }
+
+    /**
+     * 返回以node为根节点的二叉排序树中的最小节点的值
+     * 1.删除以node为根节点的二叉排序树中的最小节点（肯定是叶子节点）
+     * 2.返回该最小节点的值
+     * @param node 传入的node（当做二叉排序的根节点来查找）
+     * @return
+     */
+    public int delRightTreeMin(Node node) {
+        Node tempNode = node;
+        while (tempNode.left != null) {
+            tempNode = node.left;
+        }
+        // 找到要删除节点的父节点
+        Node parent = searchParent(tempNode.value);
+        // 要删除的targetNode节点是父节点的左子节点，还是右子节点
+        if (parent.left != null && parent.left.value == tempNode.value) {
+            // 是左子节点
+            parent.left = null;
+        } else if (parent.right != null && parent.right.value == tempNode.value) {
+            // 是右子节点
+            parent.right = null;
+        }
+        return tempNode.value;
+    }
+
+    /**
+     * 删除节点
+     * @param value
+     */
+    public void delNode(int value) {
+        if (null == root) {
+            return;
+        }
+
+        // 1.找到要删除的节点
+        Node targetNode = search(value);
+        // 找不到要删除的目标节点，则返回
+        if (null == targetNode) {
+            return;
+        }
+        // 如果找到目标节点，且当前该二叉排序就只有一个节点（即说明要删除的目标节点就是根节点），则置空根节点即可
+        if (root.left == null && root.right == null) {
+            root = null;
+            return;
+        }
+
+        // 2.找到要删除节点的父节点
+        Node parent = searchParent(value);
+
+        // 情况一：要删除的节点是叶子节点（前面已过滤只有一个节点的情况）
+        if (targetNode.left == null && targetNode.right == null) {
+            // 要删除的targetNode节点是父节点的左子节点，还是右子节点
+            if (parent.left != null && parent.left.value == targetNode.value) {
+                // 是左子节点
+                parent.left = null;
+            } else if (parent.right != null && parent.right.value == targetNode.value) {
+                // 是右子节点
+                parent.right = null;
+            }
+        } else if (targetNode.left != null && targetNode.right != null) {
+            // 情况二：删除有两颗子树的节点（包含要删除根节点）
+            int minVal = delRightTreeMin(targetNode.right);
+            targetNode.value = minVal;
+        } else {
+            // 情况三：删除只有一颗子树的节点
+            if (targetNode.left != null) {
+                // 如果targetNode有左子节点，且targetNode是parent的左子节点
+                if (parent.left.value == targetNode.value) {
+                    parent.left = targetNode.left;
+                } else {
+                    // 如果targetNode有左子节点，且targetNode是parent的右子节点
+                    parent.right = targetNode.left;
+                }
+            } else {
+                // 如果targetNode有右子节点，且targetNode是parent的左子节点
+                if (parent.left.value == targetNode.value) {
+                    parent.left = targetNode.right;
+                } else {
+                    // 如果targetNode有右子节点，且targetNode是parent的右子节点
+                    parent.right = targetNode.right;
+                }
+            }
+        }
+
+    }
+
 }
 
 /**
@@ -147,4 +274,50 @@ class Node {
             this.right.infixOrder();
         }
     }
+
+    /**
+     * 查找要删除的节点
+     * @param value 准备删除节点的值
+     * @return
+     */
+    public Node search(int value) {
+        if (this.value == value) {
+            // 当前节点就是要删除的节点
+            return this;
+        } else if (value < this.value) {
+            // 比当前节点小，则从左子树找
+            if (this.left == null) {
+                return null;
+            }
+            return this.left.search(value);
+        } else {
+            // 不小于当前节点，则从右子树找
+            if (this.right == null) {
+                return null;
+            }
+            return this.right.search(value);
+        }
+    }
+
+    /**
+     * 查找要删除节点的父节点
+     * @param value 准备删除节点的值
+     * @return
+     */
+    public Node searchParent(int value) {
+        if ((this.left != null && this.left.value == value) || (this.right != null && this.right.value == value)) {
+            // 如果左子树不为空且为要删除的节点，或者右子树不为空且为要删除的节点，就返回该节点
+            return this;
+        } else if (value < this.value && this.left != null) {
+            // 如果查找的值小于当前节点的值，且左子树不为空，则从左子树递归查找
+            return this.left.searchParent(value);
+        } else if (value >= this.value && this.right != null) {
+            // 如果查找的值不小于当前节点的值，且右子树不为空，则从右子树递归查找
+            return this.right.searchParent(value);
+        } else {
+            // 没有找到要查找值的父节点，则返回null
+            return null;
+        }
+    }
+
 }
